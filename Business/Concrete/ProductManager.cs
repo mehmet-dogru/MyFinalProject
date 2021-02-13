dda4640dc75feadcf65b5f141a4db927217cae17
Mehmet Doğru <mdogru685@gmail.com>
2021-02-13 18:53:51 +0300

    Results

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -15,14 +17,36 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
-        public List<Product> GetAll()
+
+        public IResult Add(Product product)
         {
-            return _productDal.GetAll();
+            //Business Codes
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
+            _productDal.Add(product);
+
+            return new SuccessResult(Messages.ProductAdded);
         }
 
-        public List<Product> GetAllByCategoryId(int id)
+        public IDataResult<List<Product>> GetAll()
         {
-            return _productDal.GetAll(p=>p.CategoryId==id); // Benim gönderdiğim id ile CategoryId eşit ise onları filtrele.
+            if (DateTime.Now.Hour==22)
+            {
+                return new ErrorResult();
+            }
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),true);
+        }
+
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id); // Benim gönderdiğim id ile CategoryId eşit ise onları filtrele.
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
